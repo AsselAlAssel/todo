82fa5a8f2101e27928b8e717389b7ce9fdbf97e4
AseelAlAseel <aseel1234500000@gmail.com>
2021-12-22 16:58:22 +0200

    todo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import ItemComponent from "./item";
+import  "./css.css";
+import add from "./imgs/add.png"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  item = "";
+  state = {
+    listItems: [],
+  };
+  addItem = (e) => {
+    e.preventDefault();
+    this.state.listItems.push(this.item);
+    this.item = "";
+    document.querySelector("#todo").value = "";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.setState({
+      listItems: this.state.listItems,
+    });
+  };
+  hendelCheck = (e) => {
+    this.state.listItems.splice(e.target.id,1);
+    this.setState({
+      listItems:this.state.listItems
+    })
+  };
+  handelDelete = (e) => {
+    let x =document.querySelectorAll(".title")
+   x[e.target.id].classList.toggle("delete");
+
+      };
+
+  render() {
+    return (
+      <div className="cotainer">
+        <div className="form-input">
+          <form onSubmit={this.addItem}>
+            <input
+              type="text"
+              name="toDoInput"
+              id="todo"
+              onChange={(e) => (this.item = e.target.value)}
+            />
+           <input type="image"  src={add} />
+                    </form>
+        </div>
+        <div>
+          {this.state.listItems.map((title, index) => {
+            return (
+              <ItemComponent
+                toDoTitle={title}
+                key={index}
+                id={index}
+                handelCheck={this.hendelCheck}
+                handelDelete={this.handelDelete}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/item.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from "react";
+import deleteBottun from "./imgs/delete.png";
+import checkBottun from "./imgs/check.png";
+class ItemComponent extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div  className="item">
+          <div className="title">{this.props.toDoTitle}</div>
+          <div>
+            <img
+              src={deleteBottun}
+              alt=""
+              onClick={this.props.handelDelete}
+              id={this.props.id}
+            />
+
+            <img
+              src={checkBottun}
+              alt=""
+              onClick={this.props.handelCheck}
+              id={this.props.id}
+            />
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+export default ItemComponent;
